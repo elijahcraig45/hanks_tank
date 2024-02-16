d8952607a7730258103ecdcc707be5c9c563fd31
hcraig <hcraig@manh.com>
2024-02-16 13:06:46 -0500

    Add player stats and scatterplot matrix

--- 2024_mlb/src/App.js
+++ 2024_mlb/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import TeamBatting from './components/TeamBatting';
-import PlayerStats from './components/PlayerStats';
+import PlayerBatting from './components/PlayerBatting';
+import PlayerPitching from './components/PlayerPitching';
 import AssistedAnalysis from './components/AssistedAnalysis';
 import Navbar from './components/Navbar';
 import TeamPitching from './components/TeamPitching';
@@ -15,7 +16,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/TeamBatting" element={<TeamBatting />} />
         <Route path="/TeamPitching" element={<TeamPitching />} />
-        <Route path="/PlayerStats" element={<PlayerStats />} />
+        <Route path="/PlayerBatting" element={<PlayerBatting />} />
+        <Route path="/PlayerPitching" element={<PlayerPitching />} />
         <Route path="/AssistedAnalysis" element={<AssistedAnalysis />} />
       </Routes>
     </Router>
