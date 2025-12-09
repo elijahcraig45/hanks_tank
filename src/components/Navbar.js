ca4c770a8ebc8feb7fbd014dad615eeb90d81a16
Craig, Henry <HENRY_CRAIG@homedepot.com>
2025-12-09 17:11:37 -0500

    fix: Simplify navbar with flat navigation structure
    - Removed Bootstrap NavDropdown components causing visibility issues
    - Replaced with simple Nav.Link components using React Router
    - Added direct links for all features: Stats, Comparisons, Games
    - Improved mobile responsiveness with Bootstrap Navbar.Toggle
    - Added links for new comparison tools (Season, Player, Team)
    - Better UX with clearer, always-visible navigation options

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,27 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import './styles/Navbar.css';
 
 
 function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="/">Hank's Tank 2025</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Hank's Tank 2026</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/games">Games</Nav.Link>
-            <Nav.Link href="/TeamBatting">Team Batting</Nav.Link>
-            <Nav.Link href="/TeamPitching">Team Pitching</Nav.Link>
-            <Nav.Link href="/PlayerBatting">Player Batting</Nav.Link>
-            <Nav.Link href="/PlayerPitching">Player Pitching</Nav.Link>
-            
-            
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/games">Games</Nav.Link>
+            <Nav.Link as={Link} to="/TeamBatting">Team Batting</Nav.Link>
+            <Nav.Link as={Link} to="/TeamPitching">Team Pitching</Nav.Link>
+            <Nav.Link as={Link} to="/PlayerBatting">Player Batting</Nav.Link>
+            <Nav.Link as={Link} to="/PlayerPitching">Player Pitching</Nav.Link>
+            <Nav.Link as={Link} to="/season-comparison">Season Comparison</Nav.Link>
+            <Nav.Link as={Link} to="/team-comparison">Team Comparison</Nav.Link>
+            <Nav.Link as={Link} to="/player-comparison">Player Comparison</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
